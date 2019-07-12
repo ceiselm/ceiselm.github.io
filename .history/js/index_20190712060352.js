c35af7c7d050a9c1f4395a1f874c6f78fb886fe6
Monica Ceisel <ceiselm@gmail.com>
2019-07-12 07:02:25 -0700

    Fixed footer responsiveness issues

--- .history/js/index_20190712060352.js
+++ .history/js/index_20190712060352.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
       {
         breakpoint: 480,
         settings: {
-          arrows: false,
+          arrows: true,
           centerMode: true,
           centerPadding: '40px',
           slidesToShow: 1
