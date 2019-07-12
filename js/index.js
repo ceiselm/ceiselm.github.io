0fb4140cd2cd65df89e2fa9591c3894e37f23579
Mónica Ceisel <ceiselm@gmail.com>
2019-07-11 19:39:50 -0700

    Fixed caraousel

---
+++ js/index.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('.slides').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    dots: true,
+    arrows: true,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+
+  });
+});
